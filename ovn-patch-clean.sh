f9594a6acd59ac7ff6cf2db46f2ca80c4d8ca87b
syswu <wuheng@iscas.ac.cn>
2020-08-11 23:55:45 -0400

    bug: misss name

--- ovn-patch-clean.sh
+++ ovn-patch-clean.sh
@@ -1,6 +1,8 @@
 
 for ip in `cat hosts | grep -B 100 "private" | grep -A 100 kube-master | awk '{if($0!="")print}' | grep ^[^#] | grep ^[^[]`
 do
+  echo $ip
+  name=$(cat /etc/hosts | grep $ip | awk '{print$2}')
   echo "------Node $name------"
   echo "ssh $name kubeovn-adm stop-central"
   ssh $name kubeovn-adm stop-central

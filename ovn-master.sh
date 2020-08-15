56481bf1733d3a83c5460b958901f0d335e17fca
zwjhuhu <zwjhuhu@126.com>
2020-08-15 19:05:21 +0800

    bugfix

--- ovn-master.sh
+++ ovn-master.sh
@@ -2,9 +2,9 @@
 
 ENCAP_IP=$1
 OVS_SET="ovs-vsctl set open ."
-$OVS_SET extetnal_ids:ovn-bridge=br-int
-$OVS_SET extetnal_ids:ovn-encap-type=geneve
-$OVS_SET extetnal_ids:ovn-encap-ip=$ENCAP_IP
-$OVS_SET extetnal_ids:ovn-nb=unix:/var/run/openvswitch/ovnsb_nb.sock
-$OVS_SET extetnal_ids:ovn-remote=unix:/var/run/openvswitch/ovnsb_db.sock
+$OVS_SET external_ids:ovn-bridge=br-int
+$OVS_SET external_ids:ovn-encap-type=geneve
+$OVS_SET external_ids:ovn-encap-ip=$ENCAP_IP
+$OVS_SET external_ids:ovn-nb=unix:/var/run/openvswitch/ovnsb_nb.sock
+$OVS_SET external_ids:ovn-remote=unix:/var/run/openvswitch/ovnsb_db.sock
 

8c41dbf9bc986c26fc22279f191f69df391162a2
ZhouWenjun <zwjhuhu@126.com>
2020-08-17 15:32:57 +0800

    bug

--- roles/kube-node-plus/files/ovn-node.sh
+++ roles/kube-node-plus/files/ovn-node.sh
@@ -8,6 +8,10 @@ $OVS_SET external_ids:ovn-encap-ip=$ENCAP_IP
 if [ -e /etc/ovn.conf ];then
   OVN_NB=`cat /etc/ovn.conf | grep ovnnb | awk -F= '{print $2}'`
   OVN_SB=`cat /etc/ovn.conf | grep ovnsb | awk -F= '{print $2}'`
-  $OVS_SET external_ids:ovn-nb=$OVN_NB
-  $OVS_SET external_ids:ovn-remote=$OVN_SB
+  if [ "$OVN_NB" != "" ];then
+    $OVS_SET external_ids:ovn-nb=$OVN_NB
+  fi
+  if [ "$OVN_SB" != "" ];then
+    $OVS_SET external_ids:ovn-remote=$OVN_SB
+  fi
 fi

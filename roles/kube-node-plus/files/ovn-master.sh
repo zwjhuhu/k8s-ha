f795a83d6eba58b26058ba09c087632561120836
ZhouWenjun <zwjhuhu@126.com>
2020-12-19 16:47:06 +0800

    ovs update

--- roles/kube-node-plus/files/ovn-master.sh
+++ roles/kube-node-plus/files/ovn-master.sh
@@ -5,6 +5,18 @@ OVS_SET="ovs-vsctl set open ."
 $OVS_SET external_ids:ovn-bridge=br-int
 $OVS_SET external_ids:ovn-encap-type=geneve
 $OVS_SET external_ids:ovn-encap-ip=$ENCAP_IP
-$OVS_SET external_ids:ovn-nb=unix:/var/run/openvswitch/ovnsb_nb.sock
-$OVS_SET external_ids:ovn-remote=unix:/var/run/openvswitch/ovnsb_db.sock
+if [ -e /etc/ovn.conf ];then
+  OVN_NB=`cat /etc/ovn.conf | grep ovnnb | awk -F= '{print $2}'`
+  OVN_SB=`cat /etc/ovn.conf | grep ovnsb | awk -F= '{print $2}'`
+  if [ "$OVN_NB" != "" ];then
+    $OVS_SET external_ids:ovn-nb=$OVN_NB
+  else
+    $OVS_SET external_ids:ovn-nb=unix:/var/run/openvswitch/ovnsb_nb.sock
+  fi
+  if [ "$OVN_SB" != "" ];then
+    $OVS_SET external_ids:ovn-remote=$OVN_SB
+  else
+    $OVS_SET external_ids:ovn-remote=unix:/var/run/openvswitch/ovnsb_db.sock
+  fi
+fi
 
